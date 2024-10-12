982fdf2edb33a8f0f043203730fa06e5de67f5f3
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-10-12 11:29:03 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoAPI.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String obterDados(String endereco) {
+        try {
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            return json;
+        } catch(IOException
+                | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
